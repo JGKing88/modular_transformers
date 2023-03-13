ebd3a8ed2a3127603eeb65bff6444a7cebf42201
JGKing88 <jack.g.king@gmail.com>
2023-03-13 01:28:20 -0400

    add set up capability

--- modular_transformers/models/model_utils.py
+++ modular_transformers/models/model_utils.py
@@ -1,10 +1,6 @@
 import torch
-import torch.nn as nn
+from torch import nn
 from torch.nn import functional as F
-
-import math
-
-import numpy as np
 
 class Attention(nn.Module):
     def __init__(self, n_embd, n_head, bias, dropout):
@@ -14,23 +10,24 @@ class Attention(nn.Module):
         self.key = nn.Linear(n_embd, n_embd, bias=bias)
         self.query = nn.Linear(n_embd, n_embd, bias=bias)
         self.value = nn.Linear(n_embd, n_embd, bias=bias)
-        #self.proj = nn.Linear(n_embd, n_embd, bias=bias), don't have this here, have it in block building
+        # self.proj = nn.Linear(n_embd, n_embd, bias=bias), don't have this here, have it in block building
 
         self.n_head = n_head
         self.n_embd = n_embd
         self.dropout = dropout
 
     def forward(self, x):
-        pass
+        x = self.key(x)
+        x = x.view(x.size(0), x.size(1), self.n_head, -1)
 
 
 class MLP(nn.Module):
-    def __init__(self, n_embd, bias, dropout, expansion, act):
+    def __init__(self, n_embd, bias, dropout, expansion, activation):
         super().__init__()
         self.l1 = nn.Linear(n_embd, expansion * n_embd, bias=bias)
         self.l2 = nn.Linear(expansion * n_embd, n_embd, bias=bias)
         self.n_embd = n_embd
-        self.act = act
+        self.act = activation
         self.dropout = dropout
 
     def forward(self, x):
@@ -41,6 +38,7 @@ class MLP(nn.Module):
         x = self.dropout(x)
         return x
 
+
 class Block(nn.Module):
     def __init__(self, norm, attn, mlp):
         super().__init__()
@@ -50,39 +48,40 @@ class Block(nn.Module):
         self.mlp = mlp
 
     def forward(self, x):
-        #use attn_to_mlp to get dimensions correct
-        #use self.norm to see if norm is needed
+        # use attn_to_mlp to get dimensions correct
+        # use self.norm to see if norm is needed
         x = x + self.attn(x)
         x = x + self.mlp(x)
         return x
 
-class Model(nn.Module):
 
+class Model(nn.Module):
     def __init__(self, vocab_size, block_size, n_embd, dropout, blocks):
         super().__init__()
 
         assert n_embd == blocks[0].n_embd
 
-        self.transformer = nn.ModuleDict(dict(
-            w2v = nn.Embedding(vocab_size, n_embd),
-            pos = nn.Embedding(block_size, n_embd),
-            drop = nn.Dropout(dropout),
-            blocks = blocks, #blocks will be a nn.ModuleList
-        ))
+        self.transformer = nn.ModuleDict(
+            dict(
+                w2v=nn.Embedding(vocab_size, n_embd),
+                pos=nn.Embedding(block_size, n_embd),
+                drop=nn.Dropout(dropout),
+                blocks=blocks,  # blocks will be a nn.ModuleList
+            )
+        )
         self.output_head = nn.Linear(n_embd, vocab_size, bias=False)
-    
+
     def forward(self, idx, targets):
         device = idx.device
         b, t = idx.size()
         pos = torch.arange(0, t, dtype=torch.long, device=device)
-        tok_emb = self.transformer.w2v(idx) 
-        pos_emb = self.transformer.pos(pos) 
+        tok_emb = self.transformer.w2v(idx)
+        pos_emb = self.transformer.pos(pos)
         x = self.transformer.drop(tok_emb + pos_emb)
         for block in self.transformer.blocks:
             x = block(x)
 
         logits = self.output_head(x)
-        loss = F.cross_entropy(logits.view(-1, logits.size(-1)))
+        loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets)
 
         return logits, loss
-
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "torch",
+    "numpy",
+    "pytest",
+    "black"
+]
+
+test_requirements = [
+    "pytest",
+    "pytest-timeout",
+]
+
+setup(
+    name='modular_transformers',
+    version='0.1.0',
+    description="Using modular transformers to explore human language processing",
+    long_description=readme,
+    author="Jack King",
+    author_email='jackking@mit.edu',
+    url='https://github.com/JGKing88/modular_transformers',
+    packages=find_packages(exclude=['tests']),
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='computational neuroscience, human language, '
+             'machine learning, deep neural networks, transformers',
+    test_suite='tests',
+    tests_require=test_requirements
+)
