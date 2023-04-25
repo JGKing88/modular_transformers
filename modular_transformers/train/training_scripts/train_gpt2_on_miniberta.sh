2ff82724654f3f73345d655b111a7a6c63fa9ae8
Jack G King <jackking@evnode.ib.cluster>
2023-04-25 14:08:05 -0400

    commit 2

--- modular_transformers/train/training_scripts/train_gpt2_on_miniberta.sh
+++ modular_transformers/train/training_scripts/train_gpt2_on_miniberta.sh
@@ -15,10 +15,15 @@ module load openmind/cuda/11.3
 USER_NAME=$(whoami)
 
 MT_HOME="/om2/user/${USER_NAME}/modular_transformers/"
+# MT_HOME="/om2/user/ehoseini/modular_transformers/"
 # run the .bash_profile file from USER_NAME home directory
 . /home/${USER_NAME}/.bash_profile
+# . /home/ehoseini/.bash_profile
 
-conda activate modular_transformers
+
+conda activate /om/user/ehoseini/miniconda3/envs/modular_transformers
 echo $(which python)
 
 accelerate launch --config_file "${MT_HOME}/modular_transformers/train/configs/deepspeed_config.yaml" "${MT_HOME}/modular_transformers/train/accelerate_train_gpt2.py"
+
+# accelerate launch --config_file "/om2/user/jackking/modular_transformers/modular_transformers/train/configs/deepspeed_config.yaml" "/om2/user/jackking/modular_transformers/modular_transformers/train/accelerate_train_gpt2.py"
